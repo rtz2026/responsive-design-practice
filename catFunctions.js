2f0ea34cca8c4a3847f30e58d58eda6ac28651a7
daldan <daniel.aldan@innoveo.com>
2026-03-30 14:53:39 +0200

    No duplicate images

--- catFunctions.js
+++ catFunctions.js
@@ -1,16 +1,31 @@
-async function getCatImage(numberOfCats) {
+async function getCatImage(numberOfCats, swapDirection) {
+    let imageUrl = "";
     for (let i = 0; i < numberOfCats; i++) {
         const response = await fetch("https://cataas.com/cat");
         const img = document.createElement("img");
-        img.src = response.url;
-        document.querySelector("#container").appendChild(img);
+        if (imageUrl === response.url) {
+            i--;
+            imageUrl = "";
+            console.log(i);
+        } else {
+            imageUrl = response.url;
+            img.src = imageUrl;
+            if (swapDirection) {
+                document.querySelector("#container").style.flexDirection = "row-reverse";
+                }
+
+            document.querySelector("#container").appendChild(img);
+        };
     }
 }
 
 function buttonClick() {
     document.querySelector("button").addEventListener("click", () => {
         const numberOfCats = document.getElementById("catNumber").value;
-        getCatImage(numberOfCats);
+        const swapDirection = document.getElementById("swapDirection").checked;
+        if (numberOfCats > 0 && numberOfCats <= 10) {
+            getCatImage(numberOfCats, swapDirection);
+        }
     });
 }
 
